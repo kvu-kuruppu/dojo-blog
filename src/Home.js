9ac92ec627b87aaf4a226e8bb23db449b2752e0b
Your Name <Your Email>
2022-11-26 20:35:55 +0530

    Commit 3

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
+    // let name = 'Kavindu';
+
+    const [name, setName] = useState('Kavindu');
+
+    const [age, setAge] = useState(26);
+
     const handleClick = () => {
         // console.log('Hey');
+        setName('Kuruppu');
+        setAge(30);
     };
 
     // const handleClick2 = (name, e) => {
@@ -11,6 +21,7 @@ const Home = () => {
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>Name: { name } | Age: { age }</p>
             <button onClick={handleClick}>Click me</button>
             {/* <button onClick={(e) => handleClick2('Kavindu', e)}>
                 Click me 2
