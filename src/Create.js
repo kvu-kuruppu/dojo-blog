46bb518aa0e70c4a385bbd378c5cb02774962f6a
Your Name <Your Email>
2022-11-27 17:46:30 +0530

    Commit 18

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,41 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+
+    const [body, setBody] = useState('');
+
+    const [author, setAuthor] = useState('Mario');
+
     return (
         <div className="create">
             <h2>Add new blog</h2>
+            <form>
+                <label>Blog Title: </label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog Body: </label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog Author: </label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
