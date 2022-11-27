94c151add461fe62b2d157e11351ebe5be7307a9
Your Name <Your Email>
2022-11-27 12:36:04 +0530

    Commit 10

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,17 +1,11 @@
-const BlogList = ({ blogs, title, handleDel }) => {
+const BlogList = ({ blogs, title }) => {
     return (
         <div className="blog-list">
             <h2>{title}</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                    <button
-                        className='button delete'
-                        onClick={() => handleDel(blog.id)}
-                    >
-                        Delete
-                    </button>
+                    <p>Written by {blog.author}</p>                    
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -3,35 +3,31 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setblogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'Mario', id: 1 },
-        { title: 'Welcome party', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Mario', id: 3 },
-    ]);
-
-    const [name, setName] = useState('name');
-
-    const handleDel = (id) => {
-        const newBlogs = blogs.filter((blog) => blog.id !== id);
-        setblogs(newBlogs);
-    }
+    const [blogs, setblogs] = useState(null);
 
     useEffect(() => {
-        console.log('useEffect');
-        console.log(name);
-    }, [name]);
+        fetch('http://localhost:8000/blogs')
+            .then(
+                (response) => {
+                    return response.json();
+                }
+            )
+            .then(
+                (data) => {
+                    setblogs(data);
+                });
+    }, []);
 
     return (
         <div className="home">
-            <BlogList
-                blogs={blogs}
-                title='All blogs'
-                handleDel={handleDel}
-            />
-            <button onClick={() => setName('setName')}>
-                Change Name
-            </button>
-            <p>Name: {name}</p>
+            {
+                blogs
+                &&
+                <BlogList
+                    blogs={blogs}
+                    title='All blogs'
+                />
+            }
         </div>
     );
 }
