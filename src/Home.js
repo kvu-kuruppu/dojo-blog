fef5c38371bcbba2c8b983cf947c1f7604da3944
Your Name <Your Email>
2022-11-27 13:10:05 +0530

    Commit 12

--- src/Home.js
+++ src/Home.js
@@ -2,27 +2,36 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-
     const [blogs, setblogs] = useState(null);
 
     const [isLoading, setIsLoading] = useState(true);
 
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
-            .then(
-                (response) => {
-                    return response.json();
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error('Something is not wrong!');
                 }
+                return response.json();
+            }
             )
-            .then(
-                (data) => {
-                    setblogs(data);
-                    setIsLoading(false);
-                });
+            .then((data) => {
+                setblogs(data);
+                setIsLoading(false);
+                setError(null);
+            })
+            .catch((e) => {
+                setblogs(null);
+                setIsLoading(false);
+                setError(e.message);
+            });
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isLoading && <div>Loading...</div>}
             {
                 blogs
