6de4a5d0110136203d62eaf332318bb6723e063d
Your Name <Your Email>
2022-11-27 18:14:54 +0530

    Commit 22

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import useFetch from "./useFetch";
+import { useHistory } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
@@ -7,6 +8,17 @@ const BlogDetails = () => {
     const url = `http://localhost:8000/blogs/${id}`;
 
     const { data: blog, isLoading, error } = useFetch(url);
+
+    const history = useHistory();
+
+    const handleOnClick = () => {
+        fetch(`http://localhost:8000/blogs/${blog.id}`, {
+            method: 'DELETE',
+        }).then(() => {
+            console.log('Deleted');
+            history.push('/');
+        });
+    };
 
     return (
         <div className="blog-details">
@@ -19,6 +31,11 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button
+                        className="button delete"
+                        onClick={handleOnClick}
+                    >
+                        Delete</button>
                 </article>
             }
         </div>
