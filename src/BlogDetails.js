e5fd1178df4afa90c47c29b59543e46abb904ab1
Your Name <Your Email>
2022-11-27 17:22:16 +0530

    Commit 17

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,11 +1,26 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();
 
+    const url = `http://localhost:8000/blogs/${id}`;
+
+    const { data: blog, isLoading, error } = useFetch(url);
+
     return (
         <div className="blog-details">
-            <h2>Blog Details {id}</h2>
+            {error && <div>{error}</div>}
+            {isLoading && <div>Loading...</div>}
+            {
+                blog
+                &&
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            }
         </div>
     );
 }
